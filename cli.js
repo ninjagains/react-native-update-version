6002f76c1b772575b8a0876dd7b3ffaf6cb86242
Hans-Kristian Koren <hanse@koren.im>
2022-11-07 10:07:43 +0100

    Include bundle exec in success message

--- cli.js
+++ cli.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
-const { promisify } = require('util');
-const find = require('find');
-const path = require('path');
-const fs = require('fs');
-const semver = require('semver');
-const isGitClean = require('is-git-clean');
-const meow = require('meow');
+const { promisify } = require("util");
+const find = require("find");
+const path = require("path");
+const fs = require("fs");
+const semver = require("semver");
+const isGitClean = require("is-git-clean");
+const meow = require("meow");
 
-const exec = promisify(require('child_process').exec);
+const exec = promisify(require("child_process").exec);
 const writeFile = promisify(fs.writeFile);
 const readFile = promisify(fs.readFile);
 
@@ -24,17 +24,17 @@ Options
   {
     flags: {
       silent: {
-        type: 'boolean'
+        type: "boolean",
       },
       branch: {
-        type: 'string',
-        default: 'main'
-      }
-    }
+        type: "string",
+        default: "main",
+      },
+    },
   }
 );
 
-const semverLevel = cli.input[0] || 'patch';
+const semverLevel = cli.input[0] || "patch";
 const branchName = cli.flags.branch;
 
 function log(...args) {
@@ -46,8 +46,8 @@ function log(...args) {
 }
 
 async function updateAndroidVersionName(nextVersion, buildNumber) {
-  const filename = 'android/app/build.gradle';
-  const content = await readFile(filename, 'utf8');
+  const filename = "android/app/build.gradle";
+  const content = await readFile(filename, "utf8");
   const newContent = content
     .replace(/(versionName) "([0-9].+)"/, `$1 "${nextVersion}"`)
     .replace(/(versionCode) ([0-9]+)/, `$1 ${buildNumber}`);
@@ -64,12 +64,12 @@ async function countGitCommits() {
 }
 
 function findPlistFiles() {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     find.file(
       /ios\/(\w+)\/Info.plist$/,
-      path.resolve(process.cwd(), 'ios'),
-      files => {
-        resolve(files.filter(file => !file.includes('Tests/Info.plist')));
+      path.resolve(process.cwd(), "ios"),
+      (files) => {
+        resolve(files.filter((file) => !file.includes("Tests/Info.plist")));
       }
     );
   });
@@ -84,12 +84,12 @@ async function main() {
 
   if (!isRepoClean) {
     console.error(
-      'This command requires a clean git repo. Please stash or commit any changes before re-running this command.'
+      "This command requires a clean git repo. Please stash or commit any changes before re-running this command."
     );
     return;
   }
 
-  const packageJson = JSON.parse(await readFile('package.json'));
+  const packageJson = JSON.parse(await readFile("package.json"));
   const currentVersion = packageJson.version;
   const nextVersion = semver.inc(currentVersion, semverLevel);
   const buildNumber = await countGitCommits();
@@ -97,18 +97,18 @@ async function main() {
   for (const plistFile of await findPlistFiles()) {
     await updatePlistAtKey(
       plistFile,
-      'CFBundleShortVersionString',
+      "CFBundleShortVersionString",
       nextVersion
     );
-    await updatePlistAtKey(plistFile, 'CFBundleVersion', buildNumber);
+    await updatePlistAtKey(plistFile, "CFBundleVersion", buildNumber);
   }
 
   await updateAndroidVersionName(nextVersion, buildNumber);
 
   packageJson.version = nextVersion;
-  await writeFile('package.json', JSON.stringify(packageJson, null, 2) + '\n');
+  await writeFile("package.json", JSON.stringify(packageJson, null, 2) + "\n");
 
-  await exec('git add .');
+  await exec("git add .");
   await exec(`git commit -m ${nextVersion}`);
   await exec(`git tag -a '${nextVersion}' -m '${nextVersion}'`);
 
@@ -116,10 +116,9 @@ async function main() {
     `Version numbers successfully updated to ${nextVersion} (${buildNumber}).`
   );
   log();
-  log('ios: cd ios && fastlane beta');
-  log('android: cd android && ./gradlew assembleRelease');
+  log("ios: cd ios && bundle exec fastlane beta");
+  log("android: cd android && ./gradlew assembleRelease");
   log();
-  log('Please run git push && git push --tags');
 }
 
 main();
